49a74f6457529a52fd4da77126a3f61b674e4c90
Wilbert <wilbertthxm@gmail.com>
2024-01-11 08:02:03 +0000

    Bubble sort algorithm

--- src/algorithms/bubbleSort.java
+++ src/algorithms/bubbleSort.java
@@ -1,9 +1,20 @@
 package algorithms;
 
 public class bubbleSort {
-    public void sort(){
-        for (int i = 0; i < length-1; i++) {
-            
+    public void runSort(int[] array){
+        int arraySize = array.length;
+        for (int i = 0; i < arraySize-1; i++) {
+            for (int j = 0; j < arraySize - i - 1; j++) {
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
         }
+    }
+
+    public String getName(){
+        return "Bubble Sort";
     }
 }
