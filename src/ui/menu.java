5946c9c3274d9af39ee45f4b533f91cbbc53f7e1
Wilbert <wilbertthxm@gmail.com>
2024-02-15 02:56:01 +0000

    Added time

--- src/ui/menu.java
+++ src/ui/menu.java
@@ -12,7 +12,8 @@ public class menu extends JFrame {
     private int[] array;
     private sortVisualizer visualizerPanel;
     private boolean sortingStarted = false;
-    private JComboBox<String> algorithmSelector;
+    private final JComboBox<String> algorithmSelector;
+    private final JLabel timeLabel;
 
     public menu(){
         setTitle("Algorithm Chungus");
@@ -22,30 +23,27 @@ public class menu extends JFrame {
 
         JTextField inputField = new JTextField(20);
         JButton startButton = new JButton("Run");
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if(!sortingStarted){
-                    sortingStarted = true;
-                    String inputText = inputField.getText();
-                    String[] values = inputText.split(",");
-                    array = new int[values.length];
-                    for (int i = 0; i < values.length; i++) {
-                        array[i] = Integer.parseInt(values[i].trim());
-                    }
-
-                    if(visualizerPanel != null){
-                        remove(visualizerPanel);
-                    }
-
-                    visualizerPanel = new sortVisualizer(array, getWidth(), getHeight() - 30);
-                    add(visualizerPanel, BorderLayout.CENTER);
-                    revalidate();
-                    repaint();
-                    runAlgorithm();
-                } else {
-                    sortingStarted = false;
+        startButton.addActionListener(e -> {
+            if(!sortingStarted){
+                sortingStarted = true;
+                String inputText = inputField.getText();
+                String[] values = inputText.split(",");
+                array = new int[values.length];
+                for (int i = 0; i < values.length; i++) {
+                    array[i] = Integer.parseInt(values[i].trim());
                 }
+
+                if(visualizerPanel != null){
+                    remove(visualizerPanel);
+                }
+
+                visualizerPanel = new sortVisualizer(array, getWidth(), getHeight() - 30);
+                add(visualizerPanel, BorderLayout.CENTER);
+                revalidate();
+                repaint();
+                runAlgorithm();
+            } else {
+                sortingStarted = false;
             }
         });
 
@@ -53,28 +51,34 @@ public class menu extends JFrame {
         algorithmSelector.addItem(algorithms.bubbleSort.setName());
         algorithmSelector.addItem(algorithms.selectionSort.setName());
 
+        timeLabel = new JLabel("Time: ");
 
         JPanel menuPanel = new JPanel();
         menuPanel.add(new JLabel("Enter values: "));
         menuPanel.add(inputField);
         menuPanel.add(algorithmSelector);
         menuPanel.add(startButton);
+        menuPanel.add(timeLabel);
 
         add(menuPanel, BorderLayout.NORTH);
     }
 
     private void runAlgorithm(){
-        Timer timer = new Timer(1000, new ActionListener() {
+        String selectedAlgorithm = (String) algorithmSelector.getSelectedItem();
+        assert selectedAlgorithm != null;
+        if (selectedAlgorithm.equals("Bubble Sort")) {
+            bubbleSort.runSort(array, visualizerPanel);
+        } else if (selectedAlgorithm.equals("Selection Sort")) {
+            selectionSort.runSort(array, visualizerPanel);
+        }
+        Timer timer = new Timer(0, new ActionListener() {
+            final long startTime = System.currentTimeMillis();
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Check the selected algorithm and run the corresponding sort
-                String selectedAlgorithm = (String) algorithmSelector.getSelectedItem();
-                if (selectedAlgorithm.equals("Bubble Sort")) {
-                    bubbleSort.runSort(array, visualizerPanel);
-                } else if (selectedAlgorithm.equals("Selection Sort")) {
-                    selectionSort.runSort(array, visualizerPanel);
-                }
                 ((Timer) e.getSource()).stop();
+                long endTime = System.currentTimeMillis();
+                long executionTime = endTime - startTime;
+                timeLabel.setText("Time: " + executionTime + " ms");
             }
         });
         timer.setRepeats(false);
